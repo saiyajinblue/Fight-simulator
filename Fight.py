ed4d5b9ec94c5ece2b3db2c20f87f28f10143e32
saiyajinblue <72211245+saiyajinblue@users.noreply.github.com>
2021-10-03 15:42:48 +0530

    Create Fight.py

---
+++ Fight.py
@@ -0,0 +1,17 @@
+import random
+
+vegeta = [1,3,5,7,9]
+goku = [0,2,4,6,8]
+
+v = 100
+g = 100
+
+while True:
+   num = random.randint(1,9)
+   if num in vegeta:
+      v-=20
+   else:
+      g-=20
+   print(f"vegeta power level: {v})
+   print(f"goku power level: {g})
+   break
